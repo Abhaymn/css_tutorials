0a2148c8cd5586a6ec927e25a5bb5a1af1aad7dc
abhaymn <abhaykrishnatsy007>
2023-02-20 12:38:57 +0530

    flex-box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    </ul>
+
+    <ul class="flex1">
+    <li >1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    </ul>
+
+    <ul class="flex2">
+    <li >1</li>
+    <li >2</li>
+    <li >3</li>
+    <li >4</li>
+    </ul>
+
+   
+
+</>
   );
 }
 
